3484326751eead9e4a56115c3451ccf29c5b926f
sammy-jean <Sammy.jean@icloud.com>
2022-04-03 21:52:27 -0400

    code and commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # m7-hw10-jean-samuel
+
+I had a few issue 
+
+resources 
+
+1. https://developer.mozilla.org/en-US/docs/Web/API/CookieStore 
+2. https://developer.mozilla.org/en-US/docs/Web/API/Storage/getItem 
---
+++ index.js
@@ -0,0 +1,74 @@
+// acquire references to page elements
+var nameSpan = document.querySelector("span");
+var formEl = document.querySelector("form");
+var clear = document.querySelector("#clear");
+var textArea = document.querySelector("textArea");
+
+
+
+// Retrieve name and note content from cookies and localstorage
+// Then apply them to elements on the page
+// YOUR CODE HERE
+
+
+cookieStore.get("name").then(function (cookies) {
+  if (cookies) {
+    nameSpan.noteContent = cookies.value;
+  }
+});
+
+
+
+var texts = localStorage.getItem("text");
+
+if (texts) {
+  textArea.noteContent = texts;
+} else {
+  textArea.noteContent = "";
+}
+
+
+
+
+formEl.onsubmit = function (e) {
+  // prevents form submission
+  e.preventDefault();
+  // save name element's content to cookies
+  // save textArea's content to localstorage
+  // YOUR CODE HERE
+
+  textDisplay = textArea.value;
+
+  document.cookie = "name=" + nameSpan.noteContent + ";"
+  localStorage.setItem("text", textDisplay);
+  // triggers thumbs up animation
+  this.elements.save.classList.add("emoji");
+};
+
+
+console.log(texts);
+
+clear.onclick = function () {
+  // Clear textArea's value
+  // Clear localstorage's content
+
+  // YOUR CODE HERE
+  textArea.value = '';
+  localStorage.setItem("text", '');
+  // triggers thumbs up animation
+  this.classList.add("emoji");
+};
+
+
+
+
+// this code allows repeated thumbs up animations
+function endThumbsUp() {
+  this.classList.remove("emoji");
+}
+
+
+
+
+formEl.elements.save.onanimationend = endThumbsUp;
+clear.onanimationend = endThumbsUp;
